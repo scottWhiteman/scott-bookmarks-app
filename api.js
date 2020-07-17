e4533c163a2c4d1fad8e4edbef20e0996b669eff
Scott <Scott@SCOTT-PC>
2020-07-17 07:07:27 -0700

    Removed extra files

--- api.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/scott/bookmarks'
-
-const getBookmarks = () => {
-    return fetch(`${BASE_URL}`);
-}
-
-export default {
-    getBookmarks
-};
